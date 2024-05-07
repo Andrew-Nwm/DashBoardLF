6dc1068d61f58282e286ecff6e258253a9d59ce9
Andres Rivas <162040569+Andrew-Nwm@users.noreply.github.com>
2024-05-06 20:50:55 -0500

    Uploading first files

---
+++ src/main/java/com/mycompany/lifemates_dashboard/LifeMatesDashboard.java
@@ -0,0 +1,14 @@
+package com.mycompany.lifemates_dashboard;
+
+import com.mycompany.lifemates_dashboard.igu.Principal;
+
+public class LifeMatesDashboard {
+
+    public static void main(String[] args) {
+
+        Principal principal = new Principal();
+        principal.setVisible(true);
+        principal.setLocationRelativeTo(null);
+
+    }
+}
---
+++ src/main/java/com/mycompany/lifemates_dashboard/igu/Principal.java
@@ -0,0 +1,62 @@
+package com.mycompany.lifemates_dashboard.igu;
+
+import java.awt.Image;
+import javax.swing.ImageIcon;
+
+public class Principal extends javax.swing.JFrame {
+
+    public Principal() {
+        initComponents();
+        this.setTitle("LifeMates | Dashboard");
+
+        String imagePath = "C:\\Users\\Andrew\\OneDrive\\Documentos\\NetBeansProjects\\LifeMates_Dashboard\\src\\icon.png";
+        Image IconImage = new ImageIcon(imagePath).getImage();
+        this.setIconImage(IconImage);
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextArea1 = new javax.swing.JTextArea();
+        jPanel2 = new javax.swing.JPanel();
+
+        jTextArea1.setColumns(20);
+        jTextArea1.setRows(5);
+        jScrollPane1.setViewportView(jTextArea1);
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 1080, Short.MAX_VALUE)
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 720, Short.MAX_VALUE)
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextArea jTextArea1;
+    // End of variables declaration//GEN-END:variables
+}
