f544ff777b5dcdf7404f622ac4e6e8767748934c
Andres Rivas <162040569+Andrew-Nwm@users.noreply.github.com>
2024-05-07 13:52:22 -0500

    uploading home-interface

--- src/main/java/com/mycompany/lifemates_dashboard/LifeMatesDashboard.java
+++ src/main/java/com/mycompany/lifemates_dashboard/LifeMatesDashboard.java
@@ -2,15 +2,13 @@ package com.mycompany.lifemates_dashboard;
 
 import com.mycompany.lifemates_dashboard.igu.Principal;
 
-
 public class LifeMatesDashboard {
 
     public static void main(String[] args) {
         //El siguiente código ejecuta la interfaz principal.
-        
+
         Principal principal = new Principal();
         principal.setVisible(true);
         principal.setLocationRelativeTo(null);
-
     }
 }
